c294590cfdcf623d358be891aa429aceb84fc41c
Kejsi Dhimarko <kejsi.dh@gmail.com>
2023-12-17 20:08:36 +0100

    Updated Pipeline

--- project/datapipeline.py
+++ project/datapipeline.py
@@ -23,7 +23,7 @@ def load_data(url: str, loads: int = 0) -> pd.DataFrame:
     file = None
     for _ in range(loads+1):
         try:
-            file = pd.read_csv(url, sep = 'delimiter', header = 0, delim_whitespace = True)
+            file = pd.read_csv(url, sep = 'delimiter', header = 0)
             break
         except (urllib.error.HTTPError, urllib.error.URLError):
             time.sleep(5)
