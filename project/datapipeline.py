1059bbb3ee28137eb62c745830a084d209459fe5
Kejsi Dhimarko <kejsi.dh@gmail.com>
2024-01-10 11:30:20 +0100

    Pipeline

--- project/datapipeline.py
+++ project/datapipeline.py
@@ -147,7 +147,7 @@ def main():
     ds3 = dataset3()
     ds4 = dataset4()
     
-    conn = sqlite3.connect("sqlite:////data/data.sqlite")
+    conn = sqlite3.connect("sqlite://///Users/Kejsi/made-template/data/data.sqlite")
     
     ds1.to_sql("planet_systems", conn, if_exists = "replace", index = False)
     print("Planetary Systems Data has been successfully added to SQLite")
