05e8ec3d62e124bad7907a41b44baf46d8cd9040
Kejsi Dhimarko <kejsi.dh@gmail.com>
2024-01-10 09:56:14 +0100

    Test pipeline

--- project/test_pipeline.py
+++ project/test_pipeline.py
@@ -3,6 +3,27 @@ import pandas as pd
 import sqlite3
 import datapipeline as pipe
 
+db_path = "./data/data.sqlite"
+if os.path.exists(db_path): os.remove(db_path)
+
+pipe.main()
+
+assert os.path.exists(db_path), "Database does not exist!"
+
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+table = cursor.fetchall()
+conn.close()
+
+assert ('Planetary Systems',) in table, "PS table not found!"
+assert ('Radial Velocity',) in table, "RV table not found!"
+assert ('Pulsar Timing',) in table, "PT table not found!"
+assert ('Microlensing',) in table, "ML table not found!"
+assert ('Direct Imaging',) in table, "DI table not found!"
+assert ('Transit',) in table, "TR table not found!"
+
+"""
 # Database paths for all tables
 db_ps = './planet_systems.sqlite'
 db_rv = './radial_velocity.sqlite'
@@ -88,5 +109,6 @@ conn.close()
     
 assert len(table) == 1, "Expected tables: 1 \n Found: {} tables".format(len(table))
 assert ('Transit',) in table, "TR table not found!"
+"""
     
 print("The test was executed successfully!")
